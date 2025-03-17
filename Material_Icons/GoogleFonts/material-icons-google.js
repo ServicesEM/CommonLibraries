17c722c649bb505f1cb751459b3d067699bd24b7
Services by EM <140435431+ServicesEM@users.noreply.github.com>
2025-03-17 14:24:22 -0500

    Update material-icons-google.js

--- Material_Icons/GoogleFonts/material-icons-google.js
+++ Material_Icons/GoogleFonts/material-icons-google.js
@@ -1,9 +1,13 @@
 (function() {
     const stylesheetUrl = "https://fonts.googleapis.com/icon?family=Material+Icons";
-    if (!document.querySelector(`link[rel="stylesheet"][href="${stylesheetUrl}"]`)) {
-        const linkElement = document.createElement("link");
-        linkElement.rel = "stylesheet";
-        linkElement.href = stylesheetUrl;
-        document.head.appendChild(linkElement);
+    const existingLink = document.head.querySelector('link[rel="stylesheet"]');
+
+    if (!existingLink || existingLink.getAttribute("href") !== stylesheetUrl) {
+        requestAnimationFrame(() => {
+            const linkElement = document.createElement("link");
+            linkElement.rel = "stylesheet";
+            linkElement.href = stylesheetUrl;
+            document.head.insertAdjacentElement("beforeend", linkElement);
+        });
     }
 })();
